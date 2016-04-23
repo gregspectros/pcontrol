f3542d94ea40d019ff82f763aeef0acef86118f0
gregspectros <gwright@spectrosinstruments.com>
2016-04-22 22:30:31 -0400

    Create serial_monitor_1_package_install.sh
    First steps in "Quick R Pi Install." Saves typing and cut 'n paste. Tested, works.

---
+++ serial_monitor_1_package_install.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+apt-get install git chkconfig 
+
+apt-get install libgd-gd2-perl 
+apt-get install libcgi-simple-perl 
+apt-get install libdevice-serialport-perl 
+apt-get install libxml-simple-perl 
+apt-get install libdevice-modem-perl 
+apt-get install libdbi-perl 
+apt-get install libdbd-sqlite3-perl 
+apt-get install libjson-perl 
+apt-get install libredis-perl 
+apt-get install libcrypt-ssleay-perl 
+
+apt-get install python-dev python-rpi.gpio
